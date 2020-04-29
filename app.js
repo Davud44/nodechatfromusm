e1aa1a6fd87491f7f2c2e9d9d8d0e9d56ea3e983
Davud <60606160+Davud44@users.noreply.github.com>
2020-04-30 00:47:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ io.on('connection', (socket) => {
 });
 
 
-http.listen(8000, '192.168.1.8');
+http.listen(8000);
 // const hostname = '192.168.1.8';
 // port = 8000;
 // app.listen(port, hostname);
