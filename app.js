977e7b2bf68fb4bbd2597b2e61e9fcf8f4ebbe1b
davud <davud.seyfullayev44@gmail.com>
2020-04-30 15:01:44 +0300

    daw

--- app.js
+++ app.js
@@ -172,8 +172,8 @@ io.on('connection', (socket) => {
 });
 
 http.listen(process.env.PORT);
+// http.listen(3000, '192.168.1.8');
 
-// http.listen(8000, '192.168.1.8');
 // const hostname = '192.168.1.8';
 // port = 8000;
 // app.listen(port, hostname);
