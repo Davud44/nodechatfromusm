ab69b56af85677f52bd3d077c415dd024fc68e8f
Davud <60606160+Davud44@users.noreply.github.com>
2020-04-30 13:04:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ app.set('views', path.join(__dirname, './app_server/views'))
 
 
 var connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'node_chat',
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'b7225cea6b9b86',
+    password: '41c9cf9a',
+    database: 'heroku_1f71a314b867c21',
     port: '3308'
 });
 
